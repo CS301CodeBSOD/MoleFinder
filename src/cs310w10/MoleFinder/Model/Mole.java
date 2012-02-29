80bab8ab2a08fd65187070b89db8d70fb79d04a1
Clairetic <claire.alexandria@gmail.com>
2012-02-29 15:03:42 -0700

    Added members as described by model

---
+++ src/cs310w10/MoleFinder/Model/Mole.java
@@ -0,0 +1,12 @@
+package cs310w10.MoleFinder.Model;
+
+import java.util.ArrayList;
+
+import android.graphics.Picture;
+
+public class Mole {
+	private int id;
+	private String name;
+	private String description;
+	private ArrayList<Picture> PhotoId;
+}
