809a7d859fbd73e306c3a1482c8a29661228053c
Clairetic <claire.alexandria@gmail.com>
2012-02-29 05:06:10 -0700

    Changed package name etc. to be more in line with what is expected for the course.

---
+++ src/cs310w10/MoleFinder/View/ViewActivityMain.java
@@ -0,0 +1,13 @@
+package cs310w10.MoleFinder.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ViewActivityMain extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
