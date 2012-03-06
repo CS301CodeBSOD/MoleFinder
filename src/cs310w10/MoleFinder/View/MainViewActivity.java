6059658fdd535af8acda14a154c23f250721c981
Clairetic <claire.alexandria@gmail.com>
2012-03-06 07:13:43 -0700

    More view layouts added. All views converted to RelativeLayout format as recommended in android dev tips. Added button attributes and onclicklisteners to activity class.

---
+++ src/cs310w10/MoleFinder/View/MainViewActivity.java
@@ -0,0 +1,56 @@
+package cs310w10.MoleFinder.View;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainViewActivity extends Activity {
+	private Button addMoleButton;
+	private Button viewMolesButton;
+	private Button searchLocationButton;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		addMoleButton = (Button) findViewById(R.id.MainViewAddMoleButton);
+		addMoleButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				pressAddMoleButton();
+			}
+		});
+
+		viewMolesButton = (Button) findViewById(R.id.MainViewViewMolesButton);
+		viewMolesButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				pressViewMolesButton();
+			}
+		});
+
+		searchLocationButton = (Button) findViewById(R.id.MainViewSearchLocationButton);
+		searchLocationButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				pressSearchLocationButton();
+			}
+		});
+
+	}
+
+	protected void pressViewMolesButton() {
+		// TBI
+
+	}
+
+	protected void pressSearchLocationButton() {
+		// TBI
+
+	}
+
+	public void pressAddMoleButton() {
+		// TBI
+	}
+}
