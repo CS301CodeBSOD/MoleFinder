ed6162136bde6f2902aa9dd55cfe6c6be47b209d
Clairetic <claire.alexandria@gmail.com>
2012-03-07 14:25:17 -0700

    Added IntentHelper class to help with managing the intents in our app (change views, use camera, potentially also e-mail, etc.) Also some auto-generated files have been updated.

---
+++ src/cs310w10/MoleFinder/Controller/IntentHelper.java
@@ -0,0 +1,10 @@
+package cs310w10.MoleFinder.Controller;
+
+// This class is intended to make the use of intents succinct and simple
+// in other parts of the program, since they require a lot of setup which
+// is likely to be consistent across the whole app.
+// See: http://developer.android.com/guide/topics/intents/intents-filters.html
+// for what needs to be done to implement it
+public class IntentHelper {
+
+}
