87d588352d772b2aab2276fae9870f1f99f365f0
Bing <pan3@ualberta.ca>
2012-03-16 10:41:58 -0600

    Finished Javadoc Comments for Picture and ListPicture

--- src/cs310w10/MoleFinder/Model/ListPicture.java
+++ src/cs310w10/MoleFinder/Model/ListPicture.java
@@ -5,18 +5,38 @@ import java.util.ArrayList;
 import cs310w10.MoleFinder.View.ViewActivity;
 
 
+/**
+ * This class is used to represent a list of pictures.
+ * @author Bing Pan
+ *
+ */
 public class ListPicture extends MoleModel<ViewActivity>{
 
+	/**
+	 * The list of pictures stored as an ArrayList of Picture objects.
+	 */
 	private ArrayList<Picture> pictures;
 
+	/**
+	 * Returns the list of pictures.
+	 * @return an ArrayList of Picture objects
+	 */
 	public ArrayList<Picture> getPictures() {
 		return pictures;
 	}
 
+	/**
+	 * Set the list of pictures to the list provided.
+	 * @param a list of pictures
+	 */
 	public void setPictures(ArrayList<Picture> pictures) {
 		this.pictures = pictures;
 	}
 
+	/**
+	 * Add a picture to the list.
+	 * @param a picture
+	 */
 	public void add(Picture picutre) {
 		pictures.add(picutre);
 		
--- src/cs310w10/MoleFinder/Model/Picture.java
+++ src/cs310w10/MoleFinder/Model/Picture.java
@@ -8,49 +8,110 @@ import cs310w10.MoleFinder.View.ViewActivity;
 
 import android.net.Uri;
 
+/**
+ * This class is used to represent a picture. 
+ * 
+ * @author Bing Pan
+ *
+ */
 public class Picture extends MoleModel<ViewActivity> {
+	/**
+	 * The picture's unique id which will be assigned by the database.
+	 */
 	private int id;
+	/**
+	 * A text description of the picture.
+	 */
 	private String description;
+	/**
+	 * The date when the picture was taken.
+	 */
 	private Date date;
+	/**
+	 * A list of moles with which the picture is associated with.
+	 */
 	private ArrayList<Mole> moleID;
+	/**
+	 * The picture file's URI.
+	 */
 	private Uri imageData;
 
+	/**
+	 * Return the picture's id as an integer.
+	 * @return the picture's id
+	 */
 	public int getId() {
 		return id;
 	}
 
+	/**
+	 * Set the picture's id to the integer provided.
+	 * @param the picture's id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
 
+	/**
+	 * Return the picture's description.
+	 * @return the picture's description
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Set the picture's description to the description provided.
+	 * @param description
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+	/**
+	 * Return the picture's date as a Date object.
+	 * @return the picture's date
+	 */
 	public Date getDate() {
 		return date;
 	}
 	
+	/**
+	 * Set the picture's date to the date provided.
+	 * @param date
+	 */
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
+	/**
+	 * Return a list of moles with which the picture is associated with.
+	 * @return the list of moles
+	 */
 	public ArrayList<Mole> getMoleID() {
 		return moleID;
 	}
 
+	/**
+	 * Set the list of moles the picture is associated with to the list provided.
+	 * @param an ArrayList of mole objects
+	 */
 	public void setMoleID(ArrayList<Mole> moleID) {
 		this.moleID = moleID;
 	}
 
+	/**
+	 * Return the picture's URI.
+	 * @return the picture's URI
+	 */
 	public Uri getImageData() {
 		return imageData;
 	}
 
+	/**
+	 * Set the picture's URI to the URI provided.
+	 * @param imageData
+	 */
 	public void setImageData(Uri imageData) {
 		this.imageData = imageData;
 	}
