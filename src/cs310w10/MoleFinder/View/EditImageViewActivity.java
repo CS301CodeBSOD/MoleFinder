4fa32e822868c86ed784dda2c8542a1a8dd8341a
Claire <Claire@Sapphire-W>
2012-03-17 14:44:32 -0600

    EditImageViewActivity now receives an 'id' from intent extras, which it uses to load and display a picture. Also replaced datespinner with datebutton; now works with datepicker dialog.

--- src/cs310w10/MoleFinder/View/EditImageViewActivity.java
+++ src/cs310w10/MoleFinder/View/EditImageViewActivity.java
@@ -1,11 +1,20 @@
 package cs310w10.MoleFinder.View;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -14,9 +23,11 @@ import cs310w10.MoleFinder.Controller.MoleFinderApplication;
 import cs310w10.MoleFinder.Model.Picture;
 
 public class EditImageViewActivity extends Activity implements fView<Picture> {
+	private static final int DATE_DIALOG_ID = 0;
+
 	private ImageView image;
 	private ImageButton submitButton;
-	private Spinner dateSpinner;
+	private Button dateView;
 	private EditText notesField;
 	private Picture picture;
 
@@ -34,16 +45,21 @@ public class EditImageViewActivity extends Activity implements fView<Picture> {
 			}
 		});
 
-		dateSpinner = (Spinner) findViewById(R.id.EditImageViewDateSpinner);
-		// TODO: make datepicker dialog
+		dateView = (Button) findViewById(R.id.EditImageViewDateButton);
+		dateView.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				showDialog(DATE_DIALOG_ID);
+			}
+		});
 
 		notesField = (EditText) findViewById(R.id.EditImageViewNotesField);
-
+		update(picture);
 	}
 
 	public void pressSubmitButton() {
-		// TODO: grab fields and send them to our ListMoleController, then end
-		// the intent
+		picture.setDescription(notesField.getText().toString());
+
+		// TODO: apply fields and go back to the mole you were just on.
 	}
 
 	public void update(Picture model) {
@@ -55,7 +71,44 @@ public class EditImageViewActivity extends Activity implements fView<Picture> {
 				String path = picture.getImageData().getPath();
 				Bitmap imagebitmap = BitmapFactory.decodeFile(path);
 				image.setImageBitmap(imagebitmap);
+				if (picture.getDate() != null) {
+					displayDate(picture.getDate());
+				} else {
+					picture.setDate(new Date());
+				}
 			}
 		}
+
+	}
+
+	private void displayDate(Date date) {
+		DateFormat df = new SimpleDateFormat("EEE, MMM d, ''yy");
+		String text = df.format(date);
+		dateView.setText(text);
+	}
+
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case DATE_DIALOG_ID:
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(picture.getDate());
+			return new DatePickerDialog(this,
+					new DatePickerDialog.OnDateSetListener() {
+
+						public void onDateSet(DatePicker view, int year,
+								int monthOfYear, int dayOfMonth) {
+
+							Date date = picture.getDate();
+							date.setYear(year);
+							date.setMonth(monthOfYear);
+							date.setDate(dayOfMonth);
+							picture.setDate(date);
+							displayDate(date);
+						}
+
+					}, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH)
+							, calendar.get(Calendar.DAY_OF_MONTH));
+		}
+		return null;
 	}
 }
