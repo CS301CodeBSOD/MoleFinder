f3b5b5521fdacb92fa3f2cb88ca65882f96dfcf2
Bing <pan3@ualberta.ca>
2012-04-01 14:50:18 -0600

    Updated the controllers Still working on them but most of the methods are in place

--- src/cs310w10/MoleFinder/Controller/MoleController.java
+++ src/cs310w10/MoleFinder/Controller/MoleController.java
@@ -1,44 +1,146 @@
 package cs310w10.MoleFinder.Controller;
 
+import java.util.ArrayList;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import cs310w10.MoleFinder.Model.ListMole;
 import cs310w10.MoleFinder.Model.Mole;
-import cs310w10.MoleFinder.Model.MolesDataSource;
+import cs310w10.MoleFinder.Model.TableMoles;
+import cs310w10.MoleFinder.Model.TableMolesPictures;
 
 public class MoleController {
-    public static Mole newMole(String name, String description, String location) {
-        Mole mole = new Mole();
-        mole.setName(name);
-        mole.setDescription(description);
-        mole.setLocation(location);
+	
+	private Mole mole;
+	public SQLiteDatabase database;
+	
+	public MoleController( SQLiteDatabase database){
+		this.database = database;
+	}
+	
+	public MoleController( Mole mole, SQLiteDatabase database){
+		this.mole = mole;
+		this.database = database;
+	}
+	public void createMole(String name, String description, String location){
+		mole = new Mole();
+		ContentValues values = new ContentValues();
+		values.put(TableMoles.COLUMN_NAME, name);
+		values.put(TableMoles.COLUMN_LOCATION, location);
+		values.put(TableMoles.COLUMN_DESCRIPTION, description);
 
-        //Storing mole in the database
-        MolesDataSource source = new MolesDataSource(MoleFinderApplication.getAppContext());
-        source.open();
-        mole = source.createMole(name, description, location);
-        source.close();
-        return mole;
+		long rowId = database.insert(TableMoles.TABLE_MOLES, null, values);
+		Cursor cursor = database.query(TableMoles.TABLE_MOLES, TableMoles.ALLCOLUMNS,
+				TableMoles.COLUMN_ID + " = " + rowId, null, null, null, null);
+		if (cursor.moveToFirst()){
+			mole = cursorToMole(cursor);
+		}
+	}
+
+	//delete a mole
+	/**
+	 * Delete a mole from the database.
+	 * @param the Mole object
+	 */
+	public void deleteMole(Mole mole){
+		long moleid = mole.getId();
+		database.delete(TableMoles.TABLE_MOLES, 
+				TableMoles.COLUMN_ID + " = " + moleid, null);
+	}
+
+	/**
+	 * edits a mole that has previously been set in the database
+	 * @param mole
+	 * @param name
+	 * @param description
+	 * @param location
+	 * @return
+	 */
+	public void editMole( String name, String description, String location){
+		long moleid = mole.getId();
+		ContentValues values = new ContentValues();
+		values.put(TableMoles.COLUMN_NAME, name);
+		values.put(TableMoles.COLUMN_LOCATION, location);
+		values.put(TableMoles.COLUMN_DESCRIPTION, description);
+		long rowId = database.update(TableMoles.TABLE_MOLES, values, TableMoles.COLUMN_ID + " = " + moleid, null);
+
+		Cursor cursor = database.query(TableMoles.TABLE_MOLES, TableMoles.ALLCOLUMNS,
+				TableMoles.COLUMN_ID + " = " + rowId, null, null, null, null);
+		if (cursor.moveToFirst()){
+			mole = cursorToMole(cursor);
+		}
+	}
+
+	/**
+	 * Delete all moles from the database
+	 */
+	public void deleteAllMoles(){
+		Cursor cursor = database.query(TableMoles.TABLE_MOLES, TableMoles.ALLCOLUMNS, null, null, null, null, null);
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()){
+			//Find a way to do this that resets the id field, maybe
+			Mole mole = cursorToMole(cursor);
+			deleteMole(mole);
+			cursor.moveToNext();
+		}
+		cursor.close();
+
+	}
+
+	/**
+	 * Process all the information from the row pointed by the cursor supplied and package the information into a mole object.
+	 * @param cursor
+	 * @return a Mole object
+	 */
+	private Mole cursorToMole(Cursor cursor) {
+		Mole mole = new Mole();
+		int moleId = cursor.getInt(0);
+		mole.setId(moleId);
+		mole.setName(cursor.getString(1));
+		mole.setDescription(cursor.getString(2));
+		mole.setLocation(cursor.getString(3));
+		ArrayList<Integer> photoIds = this.getPhotoIdsFromeMole(moleId);
+		mole.setPhotoId(photoIds);
+
+		return mole;
+	}
+
+
+	/**
+	 * Obtain all the ids of the pictures related to the mole provided.
+	 * @param the id of the mole
+	 * @return a list of picture ids
+	 */
+	public ArrayList<Integer> getPhotoIdsFromeMole (int moleId){
+		ArrayList<Integer> photoids = new ArrayList<Integer>();
+		String [] columns = { TableMolesPictures.COLUMN_PICTUREID };
+		Cursor cursor = database.query(TableMolesPictures.TABLE_MOLESPICTURES, columns,
+				TableMolesPictures.COLUMN_PICTUREID + " = " + moleId, null, null, null, null);
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()){
+			int photoid = cursor.getInt(0);
+			photoids.add(photoid);
+			cursor.moveToNext();
+		}
+		cursor.close();
+		return photoids; 
+	}
+	
+
+    /**
+     * Retrieve a mole from the database using its id.
+     * @param the id of the mole
+     * @return the Mole object
+     */
+    public void getMoleFromId(long id){
+        mole = new Mole();
+        Cursor cursor = database.query(TableMoles.TABLE_MOLES, TableMoles.ALLCOLUMNS, 
+                TableMoles.COLUMN_ID + " = " + String.valueOf(id), null, null, null, null);
+        cursor.moveToFirst();
+        if (!cursor.isAfterLast()){
+            mole = cursorToMole(cursor);
+        }
+        cursor.close();
     }
-
-    public static Mole editMole(String name, String description, String location, long id) {
-        Mole mole = MoleController.getMoleFromId(id);
-        mole.setName(name);
-        mole.setDescription(description);
-        mole.setLocation(location);
-
-        //Storing mole in the database
-        MolesDataSource source = new MolesDataSource(MoleFinderApplication.getAppContext());
-        source.open();
-        mole = source.editMole(mole, name, description, location);
-        source.close();
-        return mole;
-    }
-
-    public static Mole getMoleFromId(long id){
-        MolesDataSource source = new MolesDataSource(MoleFinderApplication.getAppContext());
-        source.open();
-        Mole mole = source.getMoleFromId(id);
-        source.close();
-        return mole;
-    }
-
-
 }
--- src/cs310w10/MoleFinder/Controller/PictureController.java
+++ src/cs310w10/MoleFinder/Controller/PictureController.java
@@ -6,17 +6,29 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import cs310w10.MoleFinder.Model.ListMole;
+import cs310w10.MoleFinder.Model.ListPicture;
 import cs310w10.MoleFinder.Model.Picture;
+import cs310w10.MoleFinder.Model.TableMolesPictures;
+import cs310w10.MoleFinder.Model.TablePictures;
 
 public class PictureController {
-	Picture picture;
+	private Picture picture;
+	private SQLiteDatabase database;
 
-	public PictureController(Picture picture) {
+	public PictureController(Picture picture, SQLiteDatabase database) {
 		this.picture = picture;
+		this.database = database;
 	}
 
+	public PictureController( SQLiteDatabase database) {
+		this.database = database;
+	}
 	public PictureController(Bitmap imagedata) {
 		int id = MoleFinderApplication.getListPictureController()
 				.getNextFreeID();
@@ -98,4 +110,94 @@ public class PictureController {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
+	public void createPicture(String date, String description, String uri){
+		picture = new Picture();
+		ContentValues values = new ContentValues();
+		values.put(TablePictures.COLUMN_DATE, date);
+		values.put(TablePictures.COLUMN_DESCRIPTION, description);
+		values.put(TablePictures.COLUMN_URI, uri);
+		
+        long rowId = database.insert(TablePictures.TABLE_PICTURES, null, values);
+        Cursor cursor = database.query(TablePictures.TABLE_PICTURES, TablePictures.ALLCOLUMNS,
+                TablePictures.COLUMN_ID + " = " + rowId, null, null, null, null);
+        if (cursor.moveToFirst()){
+            picture = cursorToPicture(cursor);
+        }
+	}
+    
+	/**
+	 * Delete a picture from the database
+	 * @param picture
+	 */
+	public void deletePicture(){
+		long picid = picture.getId();
+		database.delete(TablePictures.TABLE_PICTURES, 
+				TablePictures.COLUMN_ID + " = " + picid, null);
+	}
+
+	/**
+	 * @param cursor
+	 * @return
+	 */
+	private Picture cursorToPicture(Cursor cursor) {
+        Picture picture = new Picture();
+        int pictureId = cursor.getInt(0);
+        picture.setId(pictureId);
+        picture.setDescription(cursor.getString(1));
+        Calendar date = Calendar.getInstance();
+        date.setTimeInMillis(cursor.getInt(2));
+        picture.setDate(date);
+        Uri imageData = Uri.parse(cursor.getString(3));
+        picture.setImageData(imageData);
+
+        return picture;
+	} 
+
+	/**
+	 * Associate a picture with a mole and insert that relationship into the database.
+	 * @param the id of the mole
+	 * @param the id of the picture
+	 */
+	public void insertMolePicturePair ( int moleID, int PhotoID){
+		ContentValues values = new ContentValues();
+		values.put(TableMolesPictures.COLUMN_MOLEID, moleID);
+		values.put(TableMolesPictures.COLUMN_PICTUREID, PhotoID);
+
+		database.insert(TableMolesPictures.TABLE_MOLESPICTURES, null, values);
+
+	}
+
+
+	/**
+	 * Retrieve a list of picture related to the given mole.
+	 * @param the id of the mole
+	 * @return a list of pictures
+	 */
+	public ListPicture getListPictureFromMole ( int moleID ){
+		ListPicture pictures = new ListPicture();
+		String [] columns = { TableMolesPictures.COLUMN_PICTUREID };
+		Cursor cursor = database.query(TableMolesPictures.TABLE_MOLESPICTURES, columns,
+				TableMolesPictures.COLUMN_PICTUREID + " = " + moleID, null, null, null, null);
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()){
+			Picture picture = new Picture();
+			int PhotoID = cursor.getInt(0);
+			picture.setId(PhotoID);
+			//a temporal cursor to get the 
+			Cursor tempcursor = database.query(TablePictures.TABLE_PICTURES, TablePictures.ALLCOLUMNS,
+					"where " + TablePictures.COLUMN_ID + " = " + PhotoID, null, null, null, null);
+			tempcursor.moveToFirst();
+			picture.setDescription(tempcursor.getString(1));
+			Long time = (long) tempcursor.getInt(2);
+	        Calendar date = Calendar.getInstance();
+	        date.setTimeInMillis(time);
+	        picture.setDate(date);
+			picture.setImageData(Uri.parse(tempcursor.getString(3)));
+
+		}
+		return pictures;
+	}
+
 }
