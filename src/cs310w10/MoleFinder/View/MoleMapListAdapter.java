089f4e5a448a151ff25dff7dbbc9bb5c8377c166
Clairetic <claire.alexandria@gmail.com>
2012-04-05 21:42:08 -0600

    Migrated MoleMapAdapter functionality to MoleMapListAdapter and deleted MoleMapAdapter

--- src/cs310w10/MoleFinder/View/MoleMapListAdapter.java
+++ src/cs310w10/MoleFinder/View/MoleMapListAdapter.java
@@ -1,8 +1,26 @@
 package cs310w10.MoleFinder.View;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MoleMapListAdapter extends ArrayList<MoleMapAdapter> {
+import cs310w10.MoleFinder.Model.Mole;
+
+public class MoleMapListAdapter extends ArrayList<HashMap<String, String>> {
 	private static final long serialVersionUID = 1L;
+
+	public MoleMapListAdapter(ArrayList<Mole> moles) {
+		for (Mole mole : moles) {
+			add(makeMap(mole));
+		}
+	}
+
+	public HashMap<String, String> makeMap(Mole mole) {
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("name", mole.getName());
+		map.put("description", mole.getDescription());
+		map.put("id", Integer.toString(mole.getId()));
+		map.put("location", mole.getLocation());
+		return map;
+	}
 
 }
