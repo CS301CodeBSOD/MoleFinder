4bb8849db442f18c089676a70eb5d38171862ac3
Clairetic <claire.alexandria@gmail.com>
2012-04-06 05:35:25 -0600

    Created ImageAdapter for use in galleries.

---
+++ src/cs310w10/MoleFinder/View/ImageAdapter.java
@@ -0,0 +1,48 @@
+package cs310w10.MoleFinder.View;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import cs310w10.MoleFinder.Model.Picture;
+
+/**
+ * @author Claire Semple
+ * @see Adapted from Sai Geetha's blog: http://saigeethamn.blogspot
+ *      .ca/2010/05/gallery-view-android-developer-tutorial.html
+ */
+public class ImageAdapter extends BaseAdapter {
+	private final List<Picture> list;
+	private final Context context;
+
+	public ImageAdapter(Context context, List<Picture> list) {
+		this.list = list;
+		this.context = context;
+	}
+
+	public int getCount() {
+		return list.size();
+	}
+
+	public Picture getItem(int position) {
+		return list.get(position);
+	}
+
+	public long getItemId(int position) {
+		return list.get(position).getId();
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		ImageView imageview = new ImageView(context);
+		imageview.setImageURI(list.get(position).getImageData());
+		imageview.setScaleType(ImageView.ScaleType.FIT_XY);
+		imageview.setLayoutParams(new Gallery.LayoutParams(150, 120));
+		return imageview;
+
+	}
+
+}
