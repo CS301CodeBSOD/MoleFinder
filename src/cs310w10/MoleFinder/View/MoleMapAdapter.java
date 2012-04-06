93328bc7d5653ad1740e9e476ca9300048a85997
Clairetic <claire.alexandria@gmail.com>
2012-04-05 21:19:13 -0600

    Added MoleMapListAdapter, which will probably replace MoleMapAdapter

--- src/cs310w10/MoleFinder/View/MoleMapAdapter.java
+++ src/cs310w10/MoleFinder/View/MoleMapAdapter.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import cs310w10.MoleFinder.Model.Mole;
 
 public class MoleMapAdapter extends HashMap<String, String> {
+	// serves as an interface between android's built-in views and our moles
 	private static final long serialVersionUID = 1L;
 
 	public MoleMapAdapter(Mole mole) {
---
+++ src/cs310w10/MoleFinder/View/MoleMapListAdapter.java
@@ -0,0 +1,8 @@
+package cs310w10.MoleFinder.View;
+
+import java.util.ArrayList;
+
+public class MoleMapListAdapter extends ArrayList<MoleMapAdapter> {
+	private static final long serialVersionUID = 1L;
+
+}
