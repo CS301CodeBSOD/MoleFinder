0a475136a630a5adf620a8e1f693760045ca0384
Fyger <jhelberg@ualberta.ca>
2012-04-06 11:45:33 -0600

    Updated the manifest to ask permission for camera usage as well as added JavaDoc to the ListMoleController

--- src/cs310w10/MoleFinder/Controller/ListMoleController.java
+++ src/cs310w10/MoleFinder/Controller/ListMoleController.java
@@ -4,10 +4,23 @@ import java.util.ArrayList;
 
 import cs310w10.MoleFinder.Model.Mole;
 import cs310w10.MoleFinder.Model.MolesDataSource;
-
+/**
+ * @author James
+ * 
+ * List mole controller uses the MolesDataSource in order to pull moles out of the 
+ * database and pass them to the associated views. It also allows for easier storage
+ * and deletion of moles from the database
+ */
 public class ListMoleController {
+	/**
+	 * The only variable stored is a list of all the moles from the database
+	 */
 	private ArrayList<Mole> sMoles;
 
+	/**
+	 * get moles will pull all the moles from the database (reloading each call)
+	 * @return MoleList - an ArrayList of moles
+	 */
 	public ArrayList<Mole> getMoles() {
 		if (sMoles == null) {
 			sMoles = new ArrayList<Mole>();
@@ -21,6 +34,13 @@ public class ListMoleController {
 		return sMoles;
 	}
 
+	/**
+	 * stores a new mole into the database and returns the id for the mole to be assigned
+	 * @param name - of the mole
+	 * @param description - of the mole
+	 * @param location - of the mole
+	 * @return id - the new idea for the added mole in the database
+	 */
 	public int addMole(String name, String description, String location) {
 		ArrayList<Mole> moles = getMoles();
 
@@ -35,12 +55,21 @@ public class ListMoleController {
 		return newmole.getId();
 	}
 
+	/**
+	 * returns the index of the mole within the local array list of moles
+	 * @param mole - to be found
+	 * @return index - in the local arrayList
+	 */
 	public int getIndexFromMole(Mole mole) {
 		ArrayList<Mole> moles = getMoles();
 		int id = moles.indexOf(mole);
 		return id;
 	}
 
+	/**
+	 * The function will be used should the user wish to purge their database
+	 * of all the moles they have set up.
+	 */
 	public static void deleteAllMoles() {
 		MolesDataSource source = new MolesDataSource(
 				MoleFinderApplication.getAppContext());
